411f93a7b47bed38021e437ac2821b08d26cae89
Brian <459276221@qq.com>
2026-07-06 13:13:10 +0800

    Initial Unreal project

---
+++ Source/JobDemo/JobDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JobDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JobDemo, "JobDemo" );
